d5b38baa266cb733625d0b4956ab5f2f486f27d4
Viktor <viktor.svast@gmail.com>
2025-04-10 11:36:32 +0200

    dodana obrada susjeda i heuristika A* algoritma

--- astar.py
+++ astar.py
@@ -3,7 +3,7 @@ import heapq
 
 def astar_search_generator(labyrinth, start, goal, time_limit):
     """
-    Osnovna struktura A* algoritma. Logika još nije u potpunosti implementirana.
+    Implementacija A* pretrage – dodana logika za susjede i heuristika.
     """
     start_time = time.time()
     visited = set()
@@ -16,7 +16,12 @@ def astar_search_generator(labyrinth, start, goal, time_limit):
         return abs(x1 - x2) + abs(y1 - y2)
 
     def get_neighbors(x, y):
-        return []
+        possible_moves = [(0,1), (1,0), (0,-1), (-1,0)]
+        for dx, dy in possible_moves:
+            nx, ny = x + dx, y + dy
+            if 0 <= ny < len(labyrinth) and 0 <= nx < len(labyrinth[0]):
+                if labyrinth[ny][nx] == 0:
+                    yield (nx, ny)
 
     initial_priority = heuristic(start, goal)
     open_list.append((initial_priority, [start]))
@@ -35,5 +40,15 @@ def astar_search_generator(labyrinth, start, goal, time_limit):
         if current_node == goal:
             yield ("found", current_path)
             return
+
+        current_cost = cost_so_far[current_node]
+
+        for nb in get_neighbors(*current_node):
+            new_cost = current_cost + 1
+            if nb not in cost_so_far or new_cost < cost_so_far[nb]:
+                cost_so_far[nb] = new_cost
+                priority = new_cost + heuristic(nb, goal)
+                new_path = current_path + [nb]
+                heapq.heappush(open_list, (priority, new_path))
 
     yield ("no_path", )
