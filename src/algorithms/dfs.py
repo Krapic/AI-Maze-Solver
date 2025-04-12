bc9c275561040f20e65889b737dcd489167cb6d2
dantunovic1 <antunovic.damjan@gmail.com>
2025-04-12 06:31:57 +0200

    Dodana osnovna struktura DFS-a
    definirana funkcija dfs_search_generator, inicijalizirane varijable, stog te pronalazenje susjeda

--- src/algorithms/dfs.py
+++ src/algorithms/dfs.py
@@ -1,3 +1,18 @@
+import time
+
 def dfs_search_generator(labyrinth, start, goal, time_limit=None):
-    #dfs algoritam
-    pass
+
+    start_time = time.time()
+
+    def get_neighbors(x, y):
+        directions = [(0,1),(0,-1),(1,0),(-1,0)]
+        for dx, dy in directions:
+            nx, ny = x+dx, y+dy
+            if 0 <= ny < len(labyrinth) and 0 <= nx < len(labyrinth[0]):
+                if labyrinth[ny][nx] == 0:
+                    yield nx, ny
+
+    stack = []
+    stack.append(start)
+    visited = set([start])
+    parent_map = {start: None}
