65d7185ef33af8f910f60910c3b9398f85073275
Nika Nasteski <nikadrob@gmail.com>
2025-05-24 23:14:33 +0200

    Dodana statistika u bfs

--- src/algorithms/bfs.py
+++ src/algorithms/bfs.py
@@ -13,9 +13,9 @@ def bfs_search_generator(labyrinth, start, goal, time_limit=None):
     :yield: 
         ("searching", visited_set, current_node, path_so_far)
         ili 
-        ("found", final_path)
+        ("found", final_path, nodes_visited, duration)
         ili
-        ("timeout", ) / ("no_path", )  ako dođe do prekida.
+        ("timeout", nodes_visited, duration) / ("no_path", nodes_visited, duration)  ako dođe do prekida.
     """
     start_time = time.time()
 
@@ -25,7 +25,7 @@ def bfs_search_generator(labyrinth, start, goal, time_limit=None):
             nx, ny = x + dx, y + dy
             if 0 <= ny < len(labyrinth) and 0 <= nx < len(labyrinth[0]):
                 if labyrinth[ny][nx] == 0:  # slobodno polje
-                    yield nx, ny
+                    yield nx, ny 
 
     queue = deque()
     queue.append(start)
@@ -33,9 +33,11 @@ def bfs_search_generator(labyrinth, start, goal, time_limit=None):
     parent_map = {start: None}
 
     while queue:
-        if time_limit is not None and (time.time() - start_time) > time_limit:
+        duration = time.time() - start.time
+
+        if time_limit is not None and duration > time_limit:
             # "timeout" – prekidamo i javljamo da je isteklo vrijeme
-            yield ("timeout", )
+            yield ("timeout", len(visited), duration)
             return
 
         current = queue.popleft()
@@ -48,7 +50,7 @@ def bfs_search_generator(labyrinth, start, goal, time_limit=None):
         if current == goal:
             # Pronađen cilj: rekonstruišemo finalni path
             final_path = reconstruct_path(parent_map, goal)
-            yield ("found", final_path)
+            yield ("found", final_path, len(visited), duration)
             return
 
         cx, cy = current
@@ -59,7 +61,8 @@ def bfs_search_generator(labyrinth, start, goal, time_limit=None):
                 queue.append((nx, ny))
 
     # Ako smo iscrpili queue, a nismo našli cilj
-    yield ("no_path", )
+    duration = time.time() - start_time
+    yield ("no_path", len(visited), duration)
 
 
 def reconstruct_path(parent_map, end_node):
